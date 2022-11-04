0f2495c8c9b8561a5ef54a335500dada72a27454
JunEshimaP <112519106+JunEshimaP@users.noreply.github.com>
2022-11-04 15:57:09 -0400

    made output into class, same format as Input (#7)

--- src/output.py
+++ src/output.py
@@ -1,39 +1,98 @@
-# output
-
-
 import tkinter as tk
-from tkVideoPlayer import TkinterVideo
+from tkinter import ttk
+from tkVideoPlayer import *
+import datetime
+
+# sample_movies/sample.mp4
+class InteractiveOutput:
+    def __init__(self, master, moviefilename):
+        # Build a container for this plot
+        frame = tk.Frame(master)
+        self.moviefilename = moviefilename
+        # Create the tabs for the plotting
+        tab_control = ttk.Notebook(master)
+        input_tab = ttk.Frame(tab_control)
+        mesh_preview_tab = ttk.Frame(tab_control)
+        output_tab = ttk.Frame(tab_control)
+
+        tab_control.add(input_tab, text="Input")
+        tab_control.add(mesh_preview_tab, text="Mesh Preview")
+        tab_control.add(output_tab, text="Output")
+
+        self.input_tab_construction(input_tab)
+        self.mesh_preview_tab_construction(mesh_preview_tab)
+        self.output_tab_construction(output_tab)
+        tab_control.pack(expand=1, fill="both")
+
+    def input_tab_construction(self, tab):
+        # Widgets for this tab
+        message_to_user = tk.Label(tab, text="Work in progress!")
+
+        # Packing widgets
+        message_to_user.grid(row=0, column=0)
+
+    def mesh_preview_tab_construction(self, tab):
+        # Widgets for this tab
+        message_to_user = tk.Label(tab, text="Work in progress!")
+
+        # Packing widgets
+        message_to_user.grid(row=0, column=0)
+
+    def output_tab_construction(self, tab):
+
+        # bar with some text (maybe put in values of physical variables etc)
+        label = tk.Label(tab, text="Output:", fg="white", bg="black")
+        self.pp_btn = tk.Button(tab, text="play", command=self.play_pause, width=5)
+
+        # video player
+        self.videoplayer = TkinterVideo(master=tab, scaled=True)
+        self.videoplayer.load(self.moviefilename)
+        self.videoplayer.pause()
+
+        self.videoplayer.bind("<<SecondChanged>>", self.time_update)
+
+        # elapsed time
+        self.elapsed_time = tk.Label(
+            tab, text=f"elapsed = {datetime.timedelta(seconds = 0)}s", width=15
+        )
+
+        # Packing widgets
+        tab.rowconfigure([0, 1, 2, 3, 4, 5], minsize=50)
+        tab.columnconfigure([0, 1, 2], minsize=50)
+        label.grid(row=1, column=0, columnspan=3, sticky="ew")
+        self.videoplayer.grid(row=2, column=0, rowspan=3, columnspan=3, sticky="nsew")
+        self.pp_btn.grid(row=5, column=0)
+        self.elapsed_time.grid(row=5, column=2)
+
+    def time_update(self, event):
+        self.elapsed_time[
+            "text"
+        ] = f"elapsed = {str(datetime.timedelta(seconds = self.videoplayer.current_duration()))}s"
+
+    def play_pause(self):
+        # play and pause button working
+        if self.videoplayer.is_paused():
+            self.videoplayer.play()
+            self.pp_btn["text"] = "pause"
+
+        else:
+            self.videoplayer.pause()
+            self.pp_btn["text"] = "play"
 
 
-window = tk.Tk()
-label = tk.Label(text="Output:", fg="white", bg="black", width=100, height=10)
-label.pack()
+def initialize_window():
+    # Initializes a window with a title/window size/a button
+    window = tk.Tk()
+    window.title("Shallow Water Wave Equations - Numerical Simulation")
 
-window.geometry("700x700")
-
-videoplayer = TkinterVideo(master=window, scaled=True)
-videoplayer.load(r"sample_movies/sample.mp4")
-videoplayer.pack(expand=True, fill="both")
+    return window
 
 
-def play_pause():
-    """Plays and pauses videos
-    This piece of code is taken from https://github.com/PaulleDemon/tkVideoPlayer/blob/master/examples/sample_player.py
-    """
-    if videoplayer.is_paused():
-        videoplayer.play()
-        pp_btn["image"] = pause_image
-
-    else:
-        videoplayer.pause()
-        pp_btn["image"] = play_image
+def main():
+    root = initialize_window()
+    app = InteractiveOutput(root, r"sample_movies/sample.mp4")
+    root.mainloop()
 
 
-play_image = tk.PhotoImage(file=r"designs/play_button.png")
-pause_image = tk.PhotoImage(file=r"designs/pause_button.png")
-pp_btn = tk.Button(window, image=pause_image, command=play_pause)
-pp_btn.pack()
-
-videoplayer.play()  # play the video
-
-window.mainloop()
+if __name__ == "__main__":
+    main()
