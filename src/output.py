afbeb80bd9e61d91f17180fe319fa789877dc495
JunEshimaP <je2541@princeton.edu>
2022-11-09 18:49:43 -0500

    added an interactive status bar

--- src/output.py
+++ src/output.py
@@ -2,13 +2,27 @@ import tkinter as tk
 from tkinter import ttk
 from tkVideoPlayer import *
 import datetime
+import csv
+import math
 
 # sample_movies/sample.mp4
+# infofile contains information on frame rate, duration etc.
 class InteractiveOutput:
-    def __init__(self, master, moviefilename):
+    def __init__(self, master, moviefilename, infofilename):
         # Build a container for this plot
         frame = tk.Frame(master)
         self.moviefilename = moviefilename
+        self.infofilename = infofilename
+
+        # open the information file
+        # information file will have:
+        # FPS, duration
+        with open(infofilename, "r") as datafile:
+            information = csv.reader(datafile, delimiter=",")
+            first_row_data = list(information)[0]
+            self.FPS = int(first_row_data[0])
+            self.totduration = float(first_row_data[1])
+
         # Create the tabs for the plotting
         tab_control = ttk.Notebook(master)
         input_tab = ttk.Frame(tab_control)
@@ -41,7 +55,7 @@ class InteractiveOutput:
     def output_tab_construction(self, tab):
 
         # bar with some text (maybe put in values of physical variables etc)
-        label = tk.Label(tab, text="Output:", fg="white", bg="black")
+        label = tk.Label(tab, text=f"Output(FPS = {self.FPS}):", fg="white", bg="black")
         self.pp_btn = tk.Button(tab, text="play", command=self.play_pause, width=5)
 
         # video player
@@ -49,28 +63,49 @@ class InteractiveOutput:
         self.videoplayer.load(self.moviefilename)
         self.videoplayer.pause()
 
-        self.videoplayer.bind("<<SecondChanged>>", self.time_update)
+        # variable to store the current time of the video
+        self.currenttime = tk.DoubleVar(tab)
 
-        # elapsed time
-        self.elapsed_time = tk.Label(
-            tab, text=f"elapsed = {datetime.timedelta(seconds = 0)}s", width=15
+        # slider
+        self.slope_slider = tk.Scale(
+            tab,
+            variable=self.currenttime,
+            from_=0,
+            to=self.totduration,
+            digits=1,
+            resolution=1,
+            orient=tk.HORIZONTAL,
+            command=self.settime_slider,
+            tickinterval=math.floor(self.totduration / 4),
+            showvalue=0,
         )
+
+        timefromstartlabel = tk.Label(tab, textvariable=self.currenttime)
+        totdurationlabel = tk.Label(tab, text=str(self.totduration))
+
+        self.videoplayer.bind("<<SecondChanged>>", self.update_slider)
 
         # Packing widgets
         tab.rowconfigure([0, 1, 2, 3, 4, 5], minsize=50)
         tab.columnconfigure([0, 1, 2], minsize=50)
         label.grid(row=1, column=0, columnspan=3, sticky="ew")
         self.videoplayer.grid(row=2, column=0, rowspan=3, columnspan=3, sticky="nsew")
-        self.pp_btn.grid(row=5, column=0)
-        self.elapsed_time.grid(row=5, column=2)
+        timefromstartlabel.grid(row=5, column=0, padx=10, pady=10)
+        self.slope_slider.grid(row=5, column=1, padx=10, pady=10)
+        totdurationlabel.grid(row=5, column=2, padx=10, pady=10)
+        self.pp_btn.grid(row=6, column=1)
 
-    def time_update(self, event):
-        self.elapsed_time[
-            "text"
-        ] = f"elapsed = {str(datetime.timedelta(seconds = self.videoplayer.current_duration()))}s"
+    # updates current time, used to update slider
+    def update_slider(self, event):
+        self.currenttime.set(self.videoplayer.current_duration())
 
+    # change video player to the point at which the user specifies
+    def settime_slider(self, event):
+        a = self.slope_slider.get()
+        self.videoplayer.seek(int(a))
+
+    # play and pause the video
     def play_pause(self):
-        # play and pause button working
         if self.videoplayer.is_paused():
             self.videoplayer.play()
             self.pp_btn["text"] = "pause"
@@ -80,8 +115,8 @@ class InteractiveOutput:
             self.pp_btn["text"] = "play"
 
 
+# Initializes a window with a title/window size/a button
 def initialize_window():
-    # Initializes a window with a title/window size/a button
     window = tk.Tk()
     window.title("Shallow Water Wave Equations - Numerical Simulation")
 
@@ -90,7 +125,9 @@ def initialize_window():
 
 def main():
     root = initialize_window()
-    app = InteractiveOutput(root, r"sample_movies/sample.mp4")
+    app = InteractiveOutput(
+        root, r"sample_movies/sample.mp4", r"sample_movies/sampleinfo.txt"
+    )
     root.mainloop()
 
 
