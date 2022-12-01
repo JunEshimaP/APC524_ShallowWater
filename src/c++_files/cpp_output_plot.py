fe6a7a6355abf2c21031281ce1e116737bb22af9
JunEshimaP <112519106+JunEshimaP@users.noreply.github.com>
2022-12-01 11:01:33 -0500

    c++ files added (#19)

---
+++ src/c++_files/cpp_output_plot.py
@@ -0,0 +1,24 @@
+"""
+Script to plot the output from c++ file 
+(the default set up - see SWE_1D.cpp/SWE_1D.py)
+
+Uses matplotlib, deliberately made similar to moviemake class
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+h, x = np.loadtxt(
+    r"src/c++_files/h_default_cpp_end.txt", delimiter=" ", usecols=(0, 1), unpack=True
+)
+
+plt.plot(x, h)
+plt.ylim(0, 2)
+plt.xlim(-10.0, 10.0)
+plt.xlabel("x")
+plt.ylabel("height")
+plt.title(f"c++ Output of Default Set Up at T=10")
+
+# save the plot
+plt.savefig("c++_output.png")
+
+plt.show()
