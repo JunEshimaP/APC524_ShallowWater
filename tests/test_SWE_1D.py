a6703be37db9dc3dbedfb5e618cc5bfe377d7176
Zehua <zl8020@princeton.edu>
2022-12-01 19:26:03 -0500

    change tests

--- tests/test_SWE_1D.py
+++ tests/test_SWE_1D.py
@@ -10,9 +10,11 @@ FArray = NDArray[numpy.float64]
 # Parameters setting
 domainLength: float = 20.0  # meter
 xTotalNumber: int = 100
-timeLength: float = 4.0  # second
-#timeOutput: FArray = numpy.array([0.5, 1, 2, 1e8])
+timeLength: float = 10.0  # second
 FPS: int = 20
+TI = SWE_1D.RK4 # time integration method
+SD = SWE_1D.weno5 # space differentiation method
+choice: int = 4; # choice of the initial condition
 
 dx: float = domainLength / xTotalNumber
 xArray: FArray = numpy.linspace(-domainLength / 2, domainLength / 2 - dx, xTotalNumber)
@@ -21,10 +23,11 @@ h_i, hu_i = SWE_1D.inputInitialValue(xArray, xTotalNumber)
 
 def test_of_outputs():
     SWE_1D.SWE_1D(
-        dx, xArray, timeLength, xTotalNumber, FPS, SWE_1D.eulerForward, SWE_1D.centralDiff_Order2, h=h_i, hu=hu_i,
+        dx, xArray, timeLength, xTotalNumber, FPS, TI, SD, choice, h=h_i, hu=hu_i,
     )
     os.remove("Height+Momentum.png")
 
     assert filecmp.cmp("tests/benchmarks/SWE_1D_tests/output.out", "output.out", shallow=False)
     os.remove("output.out")
 
+    
